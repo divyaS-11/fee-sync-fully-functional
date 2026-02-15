e84da379b8f703eb14bf2fd30aba186cf37ab4f6
KASTHURI S <kastuhh22@gmail.com>
2026-02-15 19:55:25 +0530

    Create FeeSyncApplication.java

---
+++ backend/src/main/java/FeeSyncApplication.java
@@ -0,0 +1,12 @@
+package com.college.feesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeeSyncApplication.class, args);
+    }
+}
